9dcd3f54754f631f45bca8cd2269aafcfad133c7
Omar Abdullah <Omar@Omars-Macbook.local>
2020-04-26 00:35:30 -0400

    Website Done

---
+++ plant.js
@@ -0,0 +1,19 @@
+var ctx = document.getElementById('myChart').getContext('2d');
+var chart = new Chart(ctx, {
+    // The type of chart we want to create
+    type: 'line',
+
+    // The data for our dataset
+    data: {
+        labels: ['1', '2', '3', '4', '5'],
+        datasets: [{
+            label: 'My First dataset',
+            backgroundColor: 'rgb(26, 188, 156)',
+            borderColor: 'rgb(26, 188, 156)',
+            data: [0, 15, 10, 20, 30]
+        }]
+    },
+
+    // Configuration options go here
+    options: {}
+});
