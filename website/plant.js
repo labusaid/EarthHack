b4d8fde63a8c05d33c69cde4e772563465e9ca8a
labusaid <labusaid@gmail.com>
2020-04-26 02:33:33 -0500

    working chars from firestore

--- website/plant.js
+++ website/plant.js
@@ -1,3 +1,33 @@
+var config = {
+    apiKey: "AIzaSyBd_kch98kROmezUb8AUZVetBZBTkk4LgI",
+    authDomain: "earthxhack-2020.firebaseapp.com",
+    databaseURL: "https://earthxhack-2020.firebaseio.com",
+    projectId: "earthxhack-2020",
+    storageBucket: "earthxhack-2020.appspot.com",
+    messagingSenderId: "479859258980",
+    appId: "1:479859258980:web:54f351fec0ef4731415171"
+};
+
+firebase.initializeApp(config);
+
+var labels = []
+var values = []
+
+function getData() {
+    var db = firebase.firestore();
+    db.collection("metrics").get().then((querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+            labels.push(doc.data().label)
+            values.push(doc.data().value)
+            console.log(doc.data())
+        });
+        console.log(labels)
+        console.log(values)
+    });
+}
+
+window.addEventListener("load", getData());
+
 var ctx = document.getElementById('myChart').getContext('2d');
 var chart = new Chart(ctx, {
     // The type of chart we want to create
@@ -5,12 +35,12 @@ var chart = new Chart(ctx, {
 
     // The data for our dataset
     data: {
-        labels: ['1', '2', '3', '4', '5'],
+        labels: labels,
         datasets: [{
             label: 'Plant dataset',
             backgroundColor: 'rgb(26, 188, 156)',
             borderColor: 'rgb(26, 188, 156)',
-            data: [0, 15, 10, 20, 30]
+            data: values
         }]
     },
 
